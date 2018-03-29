1de0e5ad53091d1be19b62f5d3d466732a188821
Eduardo Gonzalez Fisher <eduardo.gonzalezfisher@kaust.edu.sa>
2018-03-29 16:24:23 +0300

    add LOCAL_IP & LOCAL_PORT as script variables

--- run-rstudio.sh
+++ run-rstudio.sh
@@ -3,6 +3,8 @@
 # VARIABLES
 IMAGE="egonzalf/rstudio-inla" # image based on rocker/rstudio
 CONTAINER_NAME=${INLA_CONTAINER_NAME:-inla-rstudio}
+LOCAL_IP="" # Define an IP address to listen, defaults to 0.0.0.0 (all interfaces)
+LOCAL_PORT="" # Define an port number to listen, defaults to 8787
 
 # MODIFY DEFAULT PASSWORD
 unset PASSWORD;
@@ -31,11 +33,14 @@ docker stop $CONTAINER_NAME 2>/dev/null
 
 docker run --rm -d --name $CONTAINER_NAME -v $_path:/home/rstudio -p 8787:8787 $IMAGE
 
+echo "Starting Docker container..."
+docker run --rm -d --name $CONTAINER_NAME -v $_path:/home/rstudio -p ${LOCAL_IP:-0.0.0.0}:${LOCAL_PORT:-8787}:8787 $IMAGE
 echo "rstudio:$PASSWORD" | docker exec -i $CONTAINER_NAME chpasswd
-
 
 # SUMMARY
 IP=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
+IP=${LOCAL_IP:-0.0.0.0}
+IP=${IP:-0.0.0.0}
 echo "================================================="
 echo "RStudio is running on:"
 echo "   http://$IP:8787/ "
@@ -43,4 +48,3 @@ echo "   http://localhost:8787/ "
 echo ""
 echo "to stop RStudio type: docker stop $CONTAINER_NAME"
 echo "================================================="
-
