e4cd66c1fed4fde09b0bbca29534c2414b7fb145
Eduardo Gonzalez Fisher <eduardo.gonzalezfisher@kaust.edu.sa>
2019-05-13 12:21:25 +0300

    build script

---
+++ build-docker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+REPO=${1:-"stable"}
+if [ "$REPO" != "stable" -a "$REPO" != "testing" ]; then
+    echo "Invalid argument! Use 'stable' or 'testing'"
+    exit -1
+fi
+
+IMAGE="egonzalf/inla-$REPO-rstudio"
+hdrfile="./.header"
+prevfile="./.last-inla-update-$REPO"
+prev=0
+
+curl -D $hdrfile "https://inla.r-inla-download.org/R/$REPO/src/contrib/PACKAGES.gz" >/dev/null 2>&1
+last=`awk -F': ' '/Last-Modi/{system("date -d\""$2"\" +%s")}' $hdrfile`
+[ -f $prevfile ] && prev=`cat $prevfile`
+
+if [ $last -gt $prev ]; then
+    echo "Building $REPO"
+    tagdate=$(date -d now +%Y%m%d)
+#    docker rmi $IMAGE:latest
+    docker build --build-arg INLA_REPO="$REPO" --build-arg TIMESTAMP=$last  -t $IMAGE:latest .
+    docker tag $IMAGE:latest $IMAGE:$tagdate
+    docker push $IMAGE:$tagdate
+    docker push $IMAGE:latest
+    echo $last > $prevfile
+else
+    echo "Up to date"
+fi
